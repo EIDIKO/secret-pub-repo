135d1d1a7f2062ca8a01305302e1cb01cf3cbc79
Tirapa Reddy Tondapu <2892046+tirapa-eidiko@users.noreply.github.com>
2023-10-17 12:20:56 +0530

    Update comment.sh

--- scripts/comment.sh
+++ scripts/comment.sh
@@ -6,7 +6,7 @@ API_URL="$BASE_URL/orgs/$REPO_NAME/secret-scanning/alerts?per_page=100&page=1&st
 echo $API_URL
 SCA_API_RESPONSE=$(curl -s -i -X GET -u "$TOKENT" "$API_URL")
 echo "::set-output name=issue-number::${ORG_OR_OWNER}"
-echo "IS $ISSUE_NUMBER"
+echo "Organization Name: $ISSUE_NUMBER"
 #echo $SCA_API_RESPONSE
    
 
