7caf035f5d77baa16eebaa6e979fed9e03259a0f
Tirapa Reddy Tondapu <2892046+tirapa-eidiko@users.noreply.github.com>
2023-10-17 11:27:51 +0530

    Update comment.sh

--- scripts/comment.sh
+++ scripts/comment.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 TOKEN="****************"         
 echo "Testing scripts $TOKENT $HOOK_ID $TOKEN $REPO_NAME $ORG_OR_OWNER $ISSUE_NUMBER"
+BASE_URL="https://api.github.com"
+API_URL="$BASE_URL/orgs/$REPO_NAME/secret-scanning/alerts?per_page=100&page=1&state=open"
+SCA_API_RESPONSE=$(curl -s -i -X GET -u "$TOKENT" "$API_URL")
+echo $SCA_API_RESPONSE
+   
 
