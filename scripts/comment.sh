f0e8c9c1f7c7281b6fb35b902f738e4a14e99a00
Tirapa Reddy Tondapu <2892046+tirapa-eidiko@users.noreply.github.com>
2023-10-17 13:14:22 +0530

    Update comment.sh

--- scripts/comment.sh
+++ scripts/comment.sh
@@ -7,7 +7,8 @@ echo "Context data(REPO_NAME): $REPO_NAME"
 echo "Context data(ORG_OR_OWNER): $ORG_OR_OWNER"
 echo $API_URL
 SCA_API_RESPONSE=$(curl -s -i -X GET -u "$TOKENT" "$API_URL")
-echo "::set-output name=owner::${ORG_OR_OWNER}"
+#echo "::set-output name=owner::${ORG_OR_OWNER}"
+echo "{owner}={$ORG_OR_OWNER}" >> $GITHUB_OUTPUT
 echo "Job Output(ORGOROWNER): $ORGOROWNER"
 #echo $SCA_API_RESPONSE
    
