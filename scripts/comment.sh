dd21034e8026bd34a25f438e54e0556583d22da6
Tirapa Reddy Tondapu <2892046+tirapa-eidiko@users.noreply.github.com>
2023-10-17 13:04:21 +0530

    Update comment.sh

--- scripts/comment.sh
+++ scripts/comment.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-TOKEN="****************"         
-echo "Testing scripts $TOKENT $HOOK_ID $TOKEN $REPO_NAME $ORG_OR_OWNER $ISSUE_NUMBER"
 API_URL="$BASE_URL/orgs/$REPO_NAME/secret-scanning/alerts?per_page=100&page=1&state=open"
+echo "Local Variable $API_URL"
+echo "Secret(Token): $TOKEN"
+echo "Env Variable(BASE_URL): $BASE_URL"
+echo "Context data(REPO_NAME): $REPO_NAME"
+echo "Context data(ORG_OR_OWNER): $ORG_OR_OWNER"
 echo $API_URL
 SCA_API_RESPONSE=$(curl -s -i -X GET -u "$TOKENT" "$API_URL")
-echo "::set-output name=issue-number::${ORG_OR_OWNER}"
-echo "Organization Name: $ISSUE_NUMBER"
+echo "::set-output name=owner::${ORG_OR_OWNER}"
+echo "Job Output(ORGOROWNER): $ORGOROWNER"
 #echo $SCA_API_RESPONSE
    
 
