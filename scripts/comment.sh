dd4e1edf4804fbed56aa35aeddcca6f2b0efef61
Tirapa Reddy Tondapu <2892046+tirapa-eidiko@users.noreply.github.com>
2023-10-17 11:22:12 +0530

    Update comment.sh

--- scripts/comment.sh
+++ scripts/comment.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 TOKEN="****************"         
-echo "Testing scripts $TOKENT $HOOK_ID $TOKEN"
+echo "Testing scripts $TOKENT $HOOK_ID $TOKEN $REPO_NAME $ORG_OR_OWNER $ISSUE_NUMBER"
+
