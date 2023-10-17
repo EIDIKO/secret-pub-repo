4d8bfa2a5e2ab283894b0247e7c0beacdb334b7d
Tirapa Reddy Tondapu <2892046+tirapa-eidiko@users.noreply.github.com>
2023-10-17 11:08:24 +0530

    Update comment.sh

--- scripts/comment.sh
+++ scripts/comment.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 TOKEN="${{ secrets.MY_PAT }}"         
-COMMENT_CONTENT=$(<table.md)
-echo $COMMENT_CONTENT
-COMMENT_BODY="|ISSUE_NUMBER|ISSUE_URL|ISSUE_TITLE|ASS_LOGIN|ASS_TYPE|\n|------------|---------|-----------|---------|--------|\n|ISSUE_NUMBER|ISSUE_URL|ISSUE_TITLE|ASS_LOGIN|ASS_TYPE|"
-curl -X POST \
-    -u "$TOKEN" \
-    -H "Accept: application/vnd.github.v3+json" \
-    "https://api.github.com/repos/EIDIKO/secret-pub-repo/issues/249/comments" \
-    -d '{
-      "body": "'"$COMMENT_BODY"'"
-    }'                    
-
+echo "Testing scripts"
